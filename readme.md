41d4dbee33b86d65ee256dabf7177c737e0c5ca3
dragoliajr <103143873+dragoliajr@users.noreply.github.com>
2022-04-19 15:00:58 -0400

    Created mortgage method and did some pratice

---
+++ readme.md
@@ -0,0 +1,24 @@
+# Read me
+
+this is our intro to java while following the video by mosh. by the end of the tutorial we'll be able to make a mortgage calculator.
+
+## variable
+a variable is what we store our data to implement.
+there's several type of variable
+* **Primitive Type**: the built-in variable types in java
+  * **int**: a number type that take up to 4 bytes of memory and store up to 2B
+  * **long**: a number type that con store a large value
+  * **float**: can store a number with decimal in it.
+  * **char**: store a character or letter
+  * **boolean**: true or false
+* **Reference Type**: the data type that were created in classes
+  * memory need to be allocated
+  * has members
+* **Escape sequences**
+  * \n create a new space
+  * \t create a tab
+  * \\ = \
+## Mortgage Method
+Created a mortgage method to showcase what we learned from the video
+    
+
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,57 @@
+import java.text.NumberFormat;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args){
-            System.out.println("hello world!");
+        mortgage();
 
-        }
+        //bunch of activity from the video
+        /*
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Age: ");
+        byte age2 = scanner.nextByte();
+        System.out.println("You are " + age2);
+
+        String result2 = NumberFormat.getPercentInstance().format(.21);
+        String message = "hello world";
+        int age = 26;
+        int[] numbers = {2,3,5,6,7,8};
+        numbers[0] = 2;
+        numbers[1] = 4;
+        numbers[2] = 5;
+        int result =  (int) Math.round(Math.random()*100);
+
+        System.out.println(result);
+        System.out.println(result2);
+        Date now = new Date();
+        //now.getTime();
+        System.out.println(now);
+
+        */
+
+    }
+    public static void mortgage(){ //Mortgage method
+        final byte Months = 12;
+        final byte percent = 100;
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Principal: ");
+        double P = scanner.nextDouble(); //Principal
+        System.out.print("Annual Interest Rate: ");
+        float AIR = scanner.nextFloat(); //Annual interest rate
+        System.out.print("period (Years) ");
+        int Years = scanner.nextInt();
+        float r = (AIR/percent)/Months; //monthly interest rate
+        int n = Years * Months; //Monthly payments
+
+        double M = P
+                * (r*Math.pow(1+r,n))
+                / (Math.pow(1+r,n)-1);
+        String result = NumberFormat.getCurrencyInstance().format(M);
+        System.out.println("Mortgage: " + result);
+
+
+
+    }
 }
